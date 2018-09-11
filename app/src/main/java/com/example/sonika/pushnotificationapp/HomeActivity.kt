39110d7c473f5f9772f4b28ed03c83cae516dc5c
sonika123 <sunshinesonika@gmail.com>
2018-09-11 12:10:14 +0545

    drawer on left

--- app/src/main/java/com/example/sonika/pushnotificationapp/HomeActivity.kt
+++ app/src/main/java/com/example/sonika/pushnotificationapp/HomeActivity.kt
@@ -1,37 +1,64 @@
 package com.example.sonika.pushnotificationapp
 
 import android.os.Bundle
-import android.view.Gravity
-import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
-import com.example.sonika.pushnotificationapp.drawer.NavigationDrawerCallbacks
 import com.example.sonika.pushnotificationapp.tabLayout.TabAdapter
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.toolbar_main.*
 
 
-class HomeActivity : AppCompatActivity(), NavigationDrawerCallbacks {
+//class HomeActivity : AppCompatActivity(), NavigationDrawerCallbacks {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setUpToolBar()
+        setUpDrawer()
         setUpViewPager()
 
         val token = FirebaseInstanceId.getInstance().token
 
-//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+//        tab_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         openFragment(HomeFragment())
 
+    }
 
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_camera -> Toast.makeText(this, "Camera clicked", Toast.LENGTH_SHORT).show()
+            R.id.nav_slideshow -> Toast.makeText(this, "Slideshow clicked", Toast.LENGTH_SHORT).show()
+            R.id.nav_gallery -> Toast.makeText(this, "Gallery clicked", Toast.LENGTH_SHORT).show()
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
 
-        setUpToolBar()
+    private fun setUpDrawer() {
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar_home, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        nav_view.setNavigationItemSelectedListener(this)
 
     }
 
@@ -52,53 +79,53 @@ class HomeActivity : AppCompatActivity(), NavigationDrawerCallbacks {
     }
 
     private fun setUpToolBar() {
-//        toolbar_home.setNavigationIcon(R.drawable.ic_navigation_icon)
-//
         setSupportActionBar(toolbar_home)
     }
 
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.nav_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.navigation_drawer -> {
-                if (drawer.isDrawerOpen(Gravity.END)) {
-                    drawer.closeDrawer(Gravity.END)
-                } else {
-                    drawer.openDrawer(Gravity.END)
-                }
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onBackPressed() {
-        if (drawer.isDrawerOpen(Gravity.END)) {
-            drawer.closeDrawer(Gravity.END)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    private fun closeDrawer() {
-        if (drawer.isDrawerOpen(Gravity.END))
-            drawer.closeDrawer(Gravity.END)
-    }
-
-    override fun onNavigationDrawerItemSelected(position: Int?) {
-        closeDrawer()
-        when (position) {
-            0 -> Toast.makeText(this, "Camera Clicked", Toast.LENGTH_SHORT).show()
-            1 -> Toast.makeText(this, "Home clicked", Toast.LENGTH_SHORT).show()
-            2 -> Toast.makeText(this, "Slideshow clicked", Toast.LENGTH_SHORT).show()
-        }
-
-    }
-
 }
+//do this for right tab_navigation drawer using menu
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.nav_menu, menu)
+//        return super.onCreateOptionsMenu(menu)
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when (item.itemId) {
+//            R.id.navigation_drawer -> {
+//                if (drawer.isDrawerOpen(Gravity.END)) {
+//                    drawer.closeDrawer(Gravity.END)
+//                } else {
+//                    drawer.openDrawer(Gravity.END)
+//                }
+//                true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+
+//    override fun onBackPressed() {
+//        if (drawer.isDrawerOpen(Gravity.END)) {
+//            drawer.closeDrawer(Gravity.END)
+//        } else {
+//            super.onBackPressed()
+//        }
+//    }
+
+//    private fun closeDrawer() {
+//        if (drawer.isDrawerOpen(Gravity.END))
+//            drawer.closeDrawer(Gravity.END)
+//    }
+//
+//    override fun onNavigationDrawerItemSelected(position: Int?) {
+//        closeDrawer()
+//        when (position) {
+//            0 -> Toast.makeText(this, "Camera Clicked", Toast.LENGTH_SHORT).show()
+//            1 -> Toast.makeText(this, "Home clicked", Toast.LENGTH_SHORT).show()
+//            2 -> Toast.makeText(this, "Slideshow clicked", Toast.LENGTH_SHORT).show()
+//        }
+//
+//    }
+
+//}
+
 
